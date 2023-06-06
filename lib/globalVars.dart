8d3da9b0b0c8f3181fad6bd8acb35940d7f92e9f
zetro-malik <zeeshanimsb@gmail.com>
2023-06-06 11:15:37 +0500

    init lecture into slide

--- lib/globalVars.dart
+++ lib/globalVars.dart
@@ -2,7 +2,7 @@ import 'package:camera/camera.dart';
 import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 
-// Color(0xFF674AEF)
+
 class GlobalVars {
   static String lectureID = "";
   static int threshold = 5;
